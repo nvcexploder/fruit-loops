ba009cb69c025efc9015f6727725ca95f632da81
nvcexploder <benacker@gmail.com>
2015-02-17 11:29:04 -0800

    move timeout function to it's own function

--- test/artifacts/dynamic-timeout.js
+++ test/artifacts/dynamic-timeout.js
@@ -1,8 +1,16 @@
 try {
-  setTimeout('try{'
-      + 'window.timeoutString="timeouted! " + ((_savedWindow === this) && (this === window) && (this === window.self));'
-      + 'window.fs = require("fs");'
-    + '} catch (err) { window.timeoutThrow = err; }', 0);
+  var self = this;
+
+  var timeoutFunction = function () {
+    try{
+      window.timeoutString="timeouted! " + ((_savedWindow === self) && (self === window) && (self === window.self));
+      window.fs = require("fs");
+    } catch (err) { 
+      window.timeoutThrow = err;
+    };
+  }
+
+  setTimeout(timeoutFunction, 0);
 } catch (err) {
   window.timeoutInitThrow = err;
 }
